f51015fac39e96e6a139b51643e858d8b078f56f
AndreasMyklebust <60507675+AndreasMyklebust@users.noreply.github.com>
2020-04-24 15:33:05 +0200

    Commit

---
+++ js/question1.js
@@ -0,0 +1,27 @@
+const form = document.querySelector("#contactForm");
+
+form.addEventListener("submit", validateInput);
+
+function validateInput(event) {
+   event.preventDefault();
+
+   const input = document.querySelector("#firstName");
+   const inputError = document.querySelector("#firstNameError");
+   const inputValue = input.value;
+
+   if (checkInputLength(inputValue) === true) {
+      inputError.style.display = "none";
+   } else {
+      inputError.style.display = "block";
+   }
+}
+
+function checkInputLength(value) {
+   const trimmedValue = value.trim();
+
+   if (trimmedValue.length > 1) {
+      return true;
+   } else {
+      return false;
+   }
+}
